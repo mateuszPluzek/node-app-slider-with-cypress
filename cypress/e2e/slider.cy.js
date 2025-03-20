6fc37832ec8005cd18b5c133bd9e790e5c8b5c53
mateusz.pluzek <mateusz.pluz@gmail.com>
2025-03-20 10:13:39 +0100

    New tests added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,30 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function() {
+  it('Checks if slider works on different devices', function () {
+    cy.visit('http://localhost:3000');
+    cy.viewport('ipad-2');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.viewport('iphone-6');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.viewport('macbook-15');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.viewport(1000, 660);
+  });
+});
